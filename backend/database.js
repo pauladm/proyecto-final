e6aa71e0e7308e73f431d06906c77fcfc3418c65
paula <pauladmontes1821@gmail.com>
2022-04-19 19:55:20 +0200

    Crear carrusel

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const mysql = require("mysql");
-
+app.use(express.static(path.join(__dirname, "ecommerce/public")));
+//Añadir si se va a usar el método POST
+app.use(express.json());
 function conectar() {
   let connection = mysql.createConnection({
     host: "localhost",
@@ -31,4 +33,21 @@ app.get("/usuarios", function (req, res) {
   });
 });
 
+app.post("/insertusuarios", function (req, res) {
+  let connection = conectar();
+  console.log(req.body);
+  let name = req.body.name;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+  let password = req.body.password;
+  connection.query(
+    "insert into usuarios(usuarios,lastName,correo,password) values(?,?,?,?)",
+    [name, lastName, email, password],
+    function (err, results, fields) {
+      if (err) {
+        res.send("error:" + err.message);
+      }
+    }
+  );
+});
 app.listen(8080);
